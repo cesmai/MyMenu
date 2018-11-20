3bd59ad2f600120b100435b89ba6321c7f25b546
cesmai <cecilemaigrot@yahoo.fr>
2018-11-20 15:41:55 +0100

    Add one fragment

--- app/src/main/java/com/example/hb/mymenu/MainActivity.kt
+++ app/src/main/java/com/example/hb/mymenu/MainActivity.kt
@@ -3,6 +3,7 @@ package com.example.hb.mymenu
 import android.os.Bundle
 import android.support.design.widget.Snackbar
 import android.support.design.widget.NavigationView
+import android.support.v4.app.Fragment
 import android.support.v4.view.GravityCompat
 import android.support.v7.app.ActionBarDrawerToggle
 import android.support.v7.app.AppCompatActivity
@@ -10,6 +11,7 @@ import android.view.Menu
 import android.view.MenuItem
 import kotlinx.android.synthetic.main.activity_main.*
 import kotlinx.android.synthetic.main.app_bar_main.*
+import org.jetbrains.anko.toast
 
 class MainActivity : AppCompatActivity(), NavigationView.OnNavigationItemSelectedListener {
 
@@ -30,6 +32,9 @@ class MainActivity : AppCompatActivity(), NavigationView.OnNavigationItemSelecte
         toggle.syncState()
 
         nav_view.setNavigationItemSelectedListener(this)
+
+        // Set Fragment
+        setFragment(OneFragment())
     }
 
     override fun onBackPressed() {
@@ -51,7 +56,14 @@ class MainActivity : AppCompatActivity(), NavigationView.OnNavigationItemSelecte
         // automatically handle clicks on the Home/Up button, so long
         // as you specify a parent activity in AndroidManifest.xml.
         when (item.itemId) {
-            R.id.action_settings -> return true
+            R.id.action_settings -> {
+                toast("select option settings")
+                return true
+            }
+            R.id.action_history -> {
+                toast("select option history")
+                return true
+            }
             else -> return super.onOptionsItemSelected(item)
         }
     }
@@ -61,25 +73,37 @@ class MainActivity : AppCompatActivity(), NavigationView.OnNavigationItemSelecte
         when (item.itemId) {
             R.id.nav_camera -> {
                 // Handle the camera action
+                toast("select navigation camera")
             }
             R.id.nav_gallery -> {
-
+                toast("select navigation gallery")
             }
             R.id.nav_slideshow -> {
-
+                toast("select navigation slideshow")
             }
             R.id.nav_manage -> {
-
+                toast("select navigation manage")
             }
             R.id.nav_share -> {
-
+                toast("select navigation share")
             }
             R.id.nav_send -> {
-
+                toast("select navigation send")
             }
         }
 
         drawer_layout.closeDrawer(GravityCompat.START)
         return true
+    }
+
+    /**
+     * Change fragment
+     */
+    private fun setFragment(frag: Fragment){
+
+        supportFragmentManager
+            .beginTransaction()
+            .replace(R.id.my_content, frag)
+            .commit()
     }
 }
