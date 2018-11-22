126338887b3f981f4fbc8392e8f5064e42ca6cd9
cesmai <cecilemaigrot@yahoo.fr>
2018-11-22 10:31:56 +0100

    Change use extension method to inflate ViewGroup

--- app/src/main/java/com/example/hb/mymenu/AndroidVersionAdapter.kt
+++ app/src/main/java/com/example/hb/mymenu/AndroidVersionAdapter.kt
@@ -1,12 +1,12 @@
 package com.example.hb.mymenu
 
+import android.support.annotation.LayoutRes
 import android.support.v7.widget.RecyclerView
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import kotlinx.android.synthetic.main.item_android_version.view.*
 
-//TODO Macha ecriture un peu obscure
 
 class AndroidVersionAdapter(val items: Array<AndroidVersion>) : RecyclerView.Adapter<AndroidVersionAdapter.ViewHolder>() {
 
@@ -21,9 +21,20 @@ class AndroidVersionAdapter(val items: Array<AndroidVersion>) : RecyclerView.Ada
 
     // Retourne la visualisation d’une ligne de la liste
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
-        val lineView = LayoutInflater.from(parent.context).inflate(R.layout.item_android_version, parent, false)
+
+        //val lineView = LayoutInflater.from(parent.context).inflate(R.layout.item_android_version, parent, false)
+
+        // OR using extension methode
+        //    cf. https://www.chillcoding.com/blog/2018/10/22/creer-liste-recyclerview-kotlin-android/ > Plus loin
+        val lineView = parent.inflate(R.layout.item_android_version)
         return ViewHolder(lineView)
     }
+
+    fun ViewGroup.inflate(@LayoutRes layoutRes: Int, attachToRoot: Boolean = false): View {
+        return LayoutInflater.from(context).inflate(layoutRes, this, attachToRoot)
+    }
+
+
 
     // Retourne le nombre d’éléments de la liste
     override fun getItemCount(): Int = items.size
