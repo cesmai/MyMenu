70b703d6aa1792609d744b7e2c8ca9b44a0c69f4
cesmai <cecilemaigrot@yahoo.fr>
2018-11-22 15:35:30 +0100

    Write on private setting file & display

--- app/src/main/java/com/example/hb/mymenu/SecondActivity.kt
+++ app/src/main/java/com/example/hb/mymenu/SecondActivity.kt
@@ -1,5 +1,7 @@
 package com.example.hb.mymenu
 
+import android.content.Context
+import android.content.SharedPreferences
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.support.v7.preference.PreferenceManager
@@ -11,13 +13,27 @@ class SecondActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_second)
 
+
         // Add a back arrow in toolbar to return to previous activity
         supportActionBar?.setDisplayHomeAsUpEnabled(true)
 
-        // Display a setting preference value
-        var defaultSharedPref = PreferenceManager.getDefaultSharedPreferences(this)
+        
+        // Display a default setting preference value
+        val defaultSharedPref = PreferenceManager.getDefaultSharedPreferences(this)
         val prefSound = defaultSharedPref.getBoolean(getString(R.string.pref_sound), true)
         toast("prefSound = $prefSound")
+
+
+        // Write on private setting file & display it
+        val sharedPref = getPreferences(Context.MODE_PRIVATE)
+        val editor: SharedPreferences.Editor = sharedPref.edit()
+        editor
+            .putInt("pouet", 5)
+            .commit() //TODO Macha consider using apply
+
+        val pouetPref = sharedPref.getInt("pouet", 0)
+        toast("pouetPref=$pouetPref")
+
     }
 
     override fun onSupportNavigateUp(): Boolean {
